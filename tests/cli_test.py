770fe26a13a42526eb28f210cf1de3caeff6f3b9
jpy-git <josephyoung.jpy@gmail.com>
2021-11-05 11:23:07 +0000

    updated flake8 config to ignore type annotations in tests/*

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -5,18 +5,26 @@ import pytest
 
 from usql_conf import cli
 
+
 @pytest.mark.parametrize(
     ["input", "expected"],
     [
-        [["pg_test"], "pg://test_user:password123@localhost/test_database?sslmode=disable\n"],
+        [
+            ["pg_test"],
+            "pg://test_user:password123@localhost/test_database?sslmode=disable\n",
+        ],
         [["my_test"], "my://test_user:password123@127.0.0.1:3306/some_db\n"],
         [["ms_test"], "ms://mr_tests:abc123@host/dbname\n"],
-    ]
+    ],
 )
 def test_main(input, expected, capsys, mocker):
     """Main returns expected outputs when valid config name is supplied."""
-    mocker.patch("usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home")
-    mocker.patch("usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd")
+    mocker.patch(
+        "usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home"
+    )
+    mocker.patch(
+        "usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd"
+    )
 
     assert not cli.main(input)
 
@@ -25,17 +33,16 @@ def test_main(input, expected, capsys, mocker):
     assert out == expected
     assert err == ""
 
-@pytest.mark.parametrize(
-    ["input"],
-    [
-        [["pg_abc"]],
-        [["my_abc"]]
-    ]
-)
+
+@pytest.mark.parametrize(["input"], [[["pg_abc"]], [["my_abc"]]])
 def test_main_config_name_not_set(input, capsys, mocker):
     """Main returns error when no config name match is found."""
-    mocker.patch("usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home")
-    mocker.patch("usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd")
+    mocker.patch(
+        "usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home"
+    )
+    mocker.patch(
+        "usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd"
+    )
     assert cli.main(input)
 
     out, err = capsys.readouterr()
@@ -43,17 +50,20 @@ def test_main_config_name_not_set(input, capsys, mocker):
     assert out == ""
     assert err == f"Config '{input[0]}' has not been set in .usql_conf\n"
 
+
 @pytest.mark.parametrize(
     ["input", "expected"],
     [
         [["pg_test"], "pg://mr_test:abc123@localhost/test_database?sslmode=disable\n"],
         [["my_test"], "my://mr_tests:abc123@127.0.0.1:3306/some_db\n"],
         [["ms_test"], "ms://mr_tests:abc123@host/dbname\n"],
-    ]
+    ],
 )
 def test_main_home_exist_only(input, expected, capsys, mocker):
-    """Connection string returned from `~/.usql_conf` if `./.usql_conf` does not exist."""
-    mocker.patch("usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home")
+    """Connection string returned from `~/.usql_conf` if `./.usql_conf` not exists."""
+    mocker.patch(
+        "usql_conf.cli.Path.home", return_value=Path(__file__).parent / "mock_home"
+    )
     mocker.patch("usql_conf.cli.Path.cwd", return_value=Path(__file__).parent)
     assert not cli.main(input)
 
@@ -62,17 +72,23 @@ def test_main_home_exist_only(input, expected, capsys, mocker):
     assert out == expected
     assert err == ""
 
+
 @pytest.mark.parametrize(
     ["input", "expected"],
     [
-        [["pg_test"], "pg://test_user:password123@localhost/test_database?sslmode=disable\n"],
+        [
+            ["pg_test"],
+            "pg://test_user:password123@localhost/test_database?sslmode=disable\n",
+        ],
         [["my_test"], "my://test_user:password123@127.0.0.1:3306/some_db\n"],
-    ]
+    ],
 )
 def test_main_cwd_exist_only(input, expected, capsys, mocker):
     """Connection string returned from `./.usql_conf` if exists."""
     mocker.patch("usql_conf.cli.Path.home", return_value=Path(__file__).parent)
-    mocker.patch("usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd")
+    mocker.patch(
+        "usql_conf.cli.Path.cwd", return_value=Path(__file__).parent / "mock_cwd"
+    )
     assert not cli.main(input)
 
     out, err = capsys.readouterr()
